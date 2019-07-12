0c01d7b65ab3cda110decc784047a07b6acf66dd
Abhishek K <abhik1698@gmail.com>
2019-07-12 18:45:30 +0530

    Phase 1 - Day 1

---
+++ upload.php
@@ -0,0 +1,71 @@
+<?php
+//Connection
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "phometo";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully to " . $_SERVER['SERVER_NAME'] . "<br>";
+$name=  $_POST['name'];
+$email=  $_POST['email'];
+$gender=  $_POST['gender'];
+
+
+//Image Upload
+$target_dir = "uploads/";
+echo $_FILES["fileToUpload"]["name"];
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+
+
+
+$sql = "INSERT INTO profile(name, email, gender, dp)
+    VALUES ('$name', '$email', '$gender', '$target_file')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+
+
+header("Location: ./view.html");
+$conn->close();
+?>
